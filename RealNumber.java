7a64a231afe5ec064623ade599bf08607ad774e6
Sophie Nichol <snichol00@stuy.edu>
2018-10-16 01:04:36 -0400

    finished product

---
+++ RealNumber.java
@@ -0,0 +1,12 @@
+public class RealNumber{
+  public double num;
+  public RealNumber(double n){
+    num = n;
+  }
+  public String toString(){
+    return " ";
+  }
+  public static void main(String[] args){
+    args = args;
+  }
+}
